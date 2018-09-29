9e3c882e2e2a95ea2024504cf864e1a2e3b636a8
PampuchSkrytozerca <stanosz.mariusz@gmail.com>
2018-09-29 14:33:36 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+
+    $('.sidenav').sidenav();
+    $('.tooltipped').tooltip();
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.datepicker').datepicker();
+    $('.timepicker').timepicker();
+    $('.scrollspy').scrollSpy();
+});
